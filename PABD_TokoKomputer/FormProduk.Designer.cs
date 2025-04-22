dd99573e5d93117f30ce30d1305245940d245e0a
Pradipa Y <merakayamsstraat@gmail.com>
2025-04-22 19:07:10 +0700

    membuat form produk

---
+++ PABD_TokoKomputer/FormProduk.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace PABD_TokoKomputer
+{
+    partial class FormProduk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtNamaProduk = new System.Windows.Forms.TextBox();
+            this.txtMerk = new System.Windows.Forms.TextBox();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(545, 128);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(243, 51);
+            this.btnHapus.TabIndex = 0;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(545, 62);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(121, 51);
+            this.btnTambah.TabIndex = 1;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(672, 62);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(116, 51);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // txtNamaProduk
+            // 
+            this.txtNamaProduk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNamaProduk.Location = new System.Drawing.Point(120, 55);
+            this.txtNamaProduk.Name = "txtNamaProduk";
+            this.txtNamaProduk.Size = new System.Drawing.Size(419, 26);
+            this.txtNamaProduk.TabIndex = 3;
+            // 
+            // txtMerk
+            // 
+            this.txtMerk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMerk.Location = new System.Drawing.Point(120, 87);
+            this.txtMerk.Name = "txtMerk";
+            this.txtMerk.Size = new System.Drawing.Size(419, 26);
+            this.txtMerk.TabIndex = 4;
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKategori.Location = new System.Drawing.Point(120, 121);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(419, 26);
+            this.txtKategori.TabIndex = 5;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHarga.Location = new System.Drawing.Point(120, 153);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(419, 26);
+            this.txtHarga.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nama Produk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Merk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Kategori";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Harga";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 194);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 244);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // FormProduk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.txtMerk);
+            this.Controls.Add(this.txtNamaProduk);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnHapus);
+            this.Name = "FormProduk";
+            this.Text = "FormProduk";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtNamaProduk;
+        private System.Windows.Forms.TextBox txtMerk;
+        private System.Windows.Forms.TextBox txtKategori;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ PABD_TokoKomputer/FormProduk.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PABD_TokoKomputer
+{
+    public partial class FormProduk : Form
+    {
+        SqlConnection conn = new SqlConnection(Database.ConnectionString);
+        int selectedID = 0;
+
+        public FormProduk()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Produk", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            if (txtNamaProduk.Text != "" && txtMerk.Text != "" && txtKategori.Text != "" && txtHarga.Text != "")
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Produk (NamaProduk, NamaMerk, KategoriProduk, Harga) VALUES (@nama, @merk, @kategori, @harga)", conn);
+                cmd.Parameters.AddWithValue("@nama", txtNamaProduk.Text);
+                cmd.Parameters.AddWithValue("@merk", txtMerk.Text);
+                cmd.Parameters.AddWithValue("@kategori", txtKategori.Text);
+                cmd.Parameters.AddWithValue("@harga", decimal.Parse(txtHarga.Text));
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                LoadData();
+                ClearInput();
+            }
+            else MessageBox.Show("Lengkapi semua input!");
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (selectedID != 0)
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Produk SET NamaProduk=@nama, NamaMerk=@merk, KategoriProduk=@kategori, Harga=@harga WHERE ProdukID=@id", conn);
+                cmd.Parameters.AddWithValue("@nama", txtNamaProduk.Text);
+                cmd.Parameters.AddWithValue("@merk", txtMerk.Text);
+                cmd.Parameters.AddWithValue("@kategori", txtKategori.Text);
+                cmd.Parameters.AddWithValue("@harga", decimal.Parse(txtHarga.Text));
+                cmd.Parameters.AddWithValue("@id", selectedID);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                LoadData();
+                ClearInput();
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (selectedID != 0)
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Produk WHERE ProdukID=@id", conn);
+                cmd.Parameters.AddWithValue("@id", selectedID);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                LoadData();
+                ClearInput();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                selectedID = Convert.ToInt32(row.Cells["ProdukID"].Value);
+                txtNamaProduk.Text = row.Cells["NamaProduk"].Value.ToString();
+                txtMerk.Text = row.Cells["NamaMerk"].Value.ToString();
+                txtKategori.Text = row.Cells["KategoriProduk"].Value.ToString();
+                txtHarga.Text = row.Cells["Harga"].Value.ToString();
+            }
+        }
+
+        private void ClearInput()
+        {
+            txtNamaProduk.Text = "";
+            txtMerk.Text = "";
+            txtKategori.Text = "";
+            txtHarga.Text = "";
+            selectedID = 0;
+        }
+    }
+}
