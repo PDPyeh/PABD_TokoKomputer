2c26b4903d6c3f67b8c850245fd92f40b29e30f0
Pradipa Y <merakayamsstraat@gmail.com>
2025-04-22 19:25:11 +0700

    membuat form pemesanan

---
+++ PABD_TokoKomputer/FormPemesanan.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace PABD_TokoKomputer
+{
+    partial class FormPemesanan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPelanggan = new System.Windows.Forms.ComboBox();
+            this.dtpTanggal = new System.Windows.Forms.DateTimePicker();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.cbProduk = new System.Windows.Forms.ComboBox();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbPelanggan
+            // 
+            this.cbPelanggan.FormattingEnabled = true;
+            this.cbPelanggan.Location = new System.Drawing.Point(18, 93);
+            this.cbPelanggan.Name = "cbPelanggan";
+            this.cbPelanggan.Size = new System.Drawing.Size(143, 21);
+            this.cbPelanggan.TabIndex = 0;
+            // 
+            // dtpTanggal
+            // 
+            this.dtpTanggal.Location = new System.Drawing.Point(18, 67);
+            this.dtpTanggal.Name = "dtpTanggal";
+            this.dtpTanggal.Size = new System.Drawing.Size(292, 20);
+            this.dtpTanggal.TabIndex = 1;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(339, 66);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(218, 48);
+            this.btnTambah.TabIndex = 3;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(563, 66);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(218, 48);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(563, 120);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(218, 48);
+            this.btnHapus.TabIndex = 5;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // cbProduk
+            // 
+            this.cbProduk.FormattingEnabled = true;
+            this.cbProduk.Location = new System.Drawing.Point(167, 93);
+            this.cbProduk.Name = "cbProduk";
+            this.cbProduk.Size = new System.Drawing.Size(143, 21);
+            this.cbProduk.TabIndex = 6;
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJumlah.Location = new System.Drawing.Point(167, 132);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(390, 29);
+            this.txtJumlah.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(763, 255);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Location = new System.Drawing.Point(18, 132);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(143, 21);
+            this.cbStatus.TabIndex = 9;
+            this.cbStatus.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(181, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 55);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Form Pemesanan";
+            // 
+            // FormPemesanan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbStatus);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.cbProduk);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.dtpTanggal);
+            this.Controls.Add(this.cbPelanggan);
+            this.Name = "FormPemesanan";
+            this.Text = "FormPemesanan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPelanggan;
+        private System.Windows.Forms.DateTimePicker dtpTanggal;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.ComboBox cbProduk;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PABD_TokoKomputer/FormPemesanan.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PABD_TokoKomputer
+{
+    public partial class FormPemesanan : Form
+    {
+        SqlConnection conn = new SqlConnection(Database.ConnectionString);
+        int selectedID = 0;
+
+        public FormPemesanan()
+        {
+            InitializeComponent();
+            LoadComboBoxData();
+            LoadData();
+            cbStatus.Items.Add("Diproses");
+            cbStatus.Items.Add("Dikirim");
+        }
+
+        private void LoadComboBoxData()
+        {
+            conn.Open();
+
+            // Pelanggan
+            SqlCommand cmd1 = new SqlCommand("SELECT PelangganID, Nama_Pelanggan FROM Pelanggan", conn);
+            SqlDataReader rdr1 = cmd1.ExecuteReader();
+            Dictionary<int, string> pelangganList = new Dictionary<int, string>();
+            while (rdr1.Read())
+            {
+                pelangganList.Add((int)rdr1["PelangganID"], rdr1["Nama_Pelanggan"].ToString());
+            }
+            rdr1.Close();
+            cbPelanggan.DataSource = new BindingSource(pelangganList, null);
+            cbPelanggan.DisplayMember = "Value";
+            cbPelanggan.ValueMember = "Key";
+
+            // Produk
+            SqlCommand cmd2 = new SqlCommand("SELECT ProdukID, NamaProduk FROM Produk", conn);
+            SqlDataReader rdr2 = cmd2.ExecuteReader();
+            Dictionary<int, string> produkList = new Dictionary<int, string>();
+            while (rdr2.Read())
+            {
+                produkList.Add((int)rdr2["ProdukID"], rdr2["NamaProduk"].ToString());
+            }
+            rdr2.Close();
+            cbProduk.DataSource = new BindingSource(produkList, null);
+            cbProduk.DisplayMember = "Value";
+            cbProduk.ValueMember = "Key";
+
+            conn.Close();
+        }
+
+        private void LoadData()
+        {
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Pemesanan", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Pemesanan (PelangganID, ProdukID, TanggalPemesanan, Status_Pesanan, Jumlah) VALUES (@pel, @prd, @tgl, @sts, @jml)", conn);
+            cmd.Parameters.AddWithValue("@pel", ((KeyValuePair<int, string>)cbPelanggan.SelectedItem).Key);
+            cmd.Parameters.AddWithValue("@prd", ((KeyValuePair<int, string>)cbProduk.SelectedItem).Key);
+            cmd.Parameters.AddWithValue("@tgl", dtpTanggal.Value);
+            cmd.Parameters.AddWithValue("@sts", cbStatus.Text);
+            cmd.Parameters.AddWithValue("@jml", int.Parse(txtJumlah.Text));
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            LoadData();
+            ClearInput();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (selectedID != 0)
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Pemesanan SET PelangganID=@pel, ProdukID=@prd, TanggalPemesanan=@tgl, Status_Pesanan=@sts, Jumlah=@jml WHERE PemesananID=@id", conn);
+                cmd.Parameters.AddWithValue("@pel", ((KeyValuePair<int, string>)cbPelanggan.SelectedItem).Key);
+                cmd.Parameters.AddWithValue("@prd", ((KeyValuePair<int, string>)cbProduk.SelectedItem).Key);
+                cmd.Parameters.AddWithValue("@tgl", dtpTanggal.Value);
+                cmd.Parameters.AddWithValue("@sts", cbStatus.Text);
+                cmd.Parameters.AddWithValue("@jml", int.Parse(txtJumlah.Text));
+                cmd.Parameters.AddWithValue("@id", selectedID);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                LoadData();
+                ClearInput();
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (selectedID != 0)
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Pemesanan WHERE PemesananID=@id", conn);
+                cmd.Parameters.AddWithValue("@id", selectedID);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                LoadData();
+                ClearInput();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                selectedID = Convert.ToInt32(row.Cells["PemesananID"].Value);
+                cbPelanggan.SelectedValue = Convert.ToInt32(row.Cells["PelangganID"].Value);
+                cbProduk.SelectedValue = Convert.ToInt32(row.Cells["ProdukID"].Value);
+                dtpTanggal.Value = Convert.ToDateTime(row.Cells["TanggalPemesanan"].Value);
+                cbStatus.Text = row.Cells["Status_Pesanan"].Value.ToString();
+                txtJumlah.Text = row.Cells["Jumlah"].Value.ToString();
+            }
+        }
+
+        private void ClearInput()
+        {
+            cbPelanggan.SelectedIndex = -1;
+            cbProduk.SelectedIndex = -1;
+            cbStatus.SelectedIndex = -1;
+            txtJumlah.Text = "";
+            dtpTanggal.Value = DateTime.Now;
+            selectedID = 0;
+        }
+    }
+}
