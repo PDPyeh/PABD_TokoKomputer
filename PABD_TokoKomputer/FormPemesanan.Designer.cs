1114b588d3838353b2e040743e5e628ef3c1fb4b
Pradipa Y <merakayamsstraat@gmail.com>
2025-04-24 14:55:04 +0700

    menambahkan label di form pemesanan

--- PABD_TokoKomputer/FormPemesanan.Designer.cs
+++ PABD_TokoKomputer/FormPemesanan.Designer.cs
@@ -38,13 +38,18 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.cbStatus = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // cbPelanggan
             // 
             this.cbPelanggan.FormattingEnabled = true;
-            this.cbPelanggan.Location = new System.Drawing.Point(18, 93);
+            this.cbPelanggan.Location = new System.Drawing.Point(18, 108);
             this.cbPelanggan.Name = "cbPelanggan";
             this.cbPelanggan.Size = new System.Drawing.Size(143, 21);
             this.cbPelanggan.TabIndex = 0;
@@ -89,17 +94,17 @@
             // cbProduk
             // 
             this.cbProduk.FormattingEnabled = true;
-            this.cbProduk.Location = new System.Drawing.Point(167, 93);
+            this.cbProduk.Location = new System.Drawing.Point(167, 108);
             this.cbProduk.Name = "cbProduk";
             this.cbProduk.Size = new System.Drawing.Size(143, 21);
             this.cbProduk.TabIndex = 6;
             // 
             // txtJumlah
             // 
-            this.txtJumlah.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtJumlah.Location = new System.Drawing.Point(167, 132);
+            this.txtJumlah.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJumlah.Location = new System.Drawing.Point(167, 156);
             this.txtJumlah.Name = "txtJumlah";
-            this.txtJumlah.Size = new System.Drawing.Size(390, 29);
+            this.txtJumlah.Size = new System.Drawing.Size(390, 20);
             this.txtJumlah.TabIndex = 7;
             // 
             // dataGridView1
@@ -113,7 +118,7 @@
             // cbStatus
             // 
             this.cbStatus.FormattingEnabled = true;
-            this.cbStatus.Location = new System.Drawing.Point(18, 132);
+            this.cbStatus.Location = new System.Drawing.Point(18, 156);
             this.cbStatus.Name = "cbStatus";
             this.cbStatus.Size = new System.Drawing.Size(143, 21);
             this.cbStatus.TabIndex = 9;
@@ -130,11 +135,61 @@
             this.label1.Text = "Form Pemesanan";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Tanggal Pemesanan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nama Pelanggan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Produk";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Status Pemesanan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Jumlah Pesanan";
+            // 
             // FormPemesanan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbStatus);
             this.Controls.Add(this.dataGridView1);
@@ -166,5 +221,10 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ComboBox cbStatus;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
