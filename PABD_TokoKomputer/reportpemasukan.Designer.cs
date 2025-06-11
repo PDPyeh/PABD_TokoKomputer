b0e9d40fed722741104c9499727a03c0240fcebb
Pradipa Y <merakayamsstraat@gmail.com>
2025-06-11 14:27:09 +0700

    changing design

--- PABD_TokoKomputer/reportpemasukan.Designer.cs
+++ PABD_TokoKomputer/reportpemasukan.Designer.cs
@@ -43,6 +43,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(857, 450);
             this.Controls.Add(this.reportViewer1);
             this.Name = "reportpemasukan";
