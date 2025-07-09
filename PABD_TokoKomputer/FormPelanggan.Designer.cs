6fcbbf7e05051b008cd6f42a56a34e739991e9aa
Pradipa Y <merakayamsstraat@gmail.com>
2025-07-09 17:33:13 +0700

    chasges

--- PABD_TokoKomputer/FormPelanggan.Designer.cs
+++ PABD_TokoKomputer/FormPelanggan.Designer.cs
@@ -136,6 +136,8 @@ namespace UCP1PABD
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(766, 270);
             this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // label4
             // 
